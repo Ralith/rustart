b4d1c12400c0a878bba3c6baed6dfaa73faf99d8
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-11-09 02:08:51 -0800

    Recoverable seeds, more operators

--- src/expr.rs
+++ src/expr.rs
@@ -1,25 +1,21 @@
 use std::ops;
+use std::iter;
 use std::fmt;
 
 use rand::{Rand, Rng, Closed01};
 use na;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Expr {
     Constant(Color),
     Radial,
+    Square,
+    Cos,
     Transform(Box<Expr>, na::Affine2<f32>),
     Multiply(Box<Expr>, Box<Expr>),
+    Average(Vec<Expr>),
     Invert(Box<Expr>),
     Tile(Box<Expr>),
-}
-
-fn to_unit_square(p: na::Point2<f32>) -> na::Point2<f32> {
-    na::Point2::new((p.x + 1.0) / 2.0, (p.y + 1.0) / 2.0)
-}
-
-fn from_unit_square(p: na::Point2<f32>) -> na::Point2<f32> {
-    na::Point2::new((p.x * 2.0) - 1.0, (p.y * 2.0) - 1.0)
 }
 
 fn wrap(x: f32, min: f32, max: f32) -> f32 {
@@ -33,14 +29,50 @@ impl Expr {
         match *self {
             Constant(x) => x,
             Radial => Color::gray((p.x.powi(2) + p.y.powi(2)).sqrt()),
+            Square => Color::gray((p.x.abs() + p.y.abs()) / 2.0),
+            Cos => Color::gray((p.x.cos() + 1.0) / 2.0),
             Transform(ref e, ref xf) => e.eval(xf * p),
             Multiply(ref e, ref f) => e.eval(p) * f.eval(p),
+            Average(ref es) => es.iter().map(|e| e.eval(p)).sum::<Color>() / (es.len() as f32),
             Invert(ref e) => -e.eval(p),
             Tile(ref e) => e.eval(na::Point2::new(wrap(p.x, -1.0, 1.0),
                                                   wrap(p.y, -1.0, 1.0)))
         }
     }
+
+    pub fn simplify(&self) -> Expr {
+        use self::Expr::*;
+        match *self {
+            Transform(ref e, ref xf) => Transform(Box::new(e.simplify()), *xf),
+            Multiply(ref e, ref f) => Multiply(Box::new(e.simplify()), Box::new(f.simplify())),
+            Invert(ref e) => match **e {
+                ref f@Invert(_) => match f.simplify() { Invert(x) => *x, x => Invert(Box::new(x)) },
+                ref x => Invert(Box::new(x.simplify())),
+            }
+            Tile(ref e) => match **e {
+                Constant(c) => Constant(c),
+                ref e => Tile(Box::new(e.simplify())),
+            }
+            Average(ref es) => Average(es.iter().flat_map(|e| match *e {
+                ref e@Average(_) => match e.simplify() { Average(es) => es.into_iter(), e => vec![e].into_iter() },
+                ref e => vec![e.simplify()].into_iter(),
+            }).collect()),
+            ref x => x.clone(),
+        }
+    }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Expr::*;
+
+    #[test]
+    fn simplify_invert() {
+        assert_eq!(Invert(Box::new(Invert(Box::new(Invert(Box::new(Invert(Box::new(Invert(Box::new(Radial)))))))))).simplify(),
+                   Invert(Box::new(Radial)));
+    }
+}
+
 
 impl fmt::Display for Expr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -48,15 +80,28 @@ impl fmt::Display for Expr {
         match *self {
             Constant(x) => x.fmt(f),
             Radial => f.pad("radial"),
-            Transform(ref e, ref xf) => write!(f, "xf({})", e),
-            Multiply(ref e, ref g) => write!(f, "{} * {}", e, g),
+            Square => f.pad("square"),
+            Cos => f.pad("cos"),
+            Transform(ref e, _) => write!(f, "xf({})", e),
+            Multiply(ref e, ref g) => write!(f, "mul({}, {})", e, g),
+            Average(ref es) => {
+                let mut iter = es.iter();
+                f.write_str("avg(")?;
+                iter.next().unwrap().fmt(f)?;
+                while let Some(e) = iter.next() {
+                    f.write_str(", ")?;
+                    e.fmt(f)?;
+                }
+                f.write_str(")")?;
+                Ok(())
+            }
             Invert(ref e) => write!(f, "-{}", e),
             Tile(ref e) => write!(f, "tile({})", e),
         }
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Color(pub [f32; 3]);
 
 impl Color {
@@ -68,9 +113,27 @@ impl ops::Mul for Color {
     fn mul(self, rhs: Color) -> Color { Color([self.0[0] * rhs.0[0], self.0[1] * rhs.0[1], self.0[2] * rhs.0[2]]) }
 }
 
+impl ops::Add for Color {
+    type Output = Color;
+    fn add(self, rhs: Color) -> Color { Color([self.0[0] + rhs.0[0], self.0[1] + rhs.0[1], self.0[2] + rhs.0[2]]) }
+}
+
+impl ops::Div<f32> for Color {
+    type Output = Color;
+    fn div(self, rhs: f32) -> Color { Color([self.0[0] / rhs, self.0[1] / rhs, self.0[2] / rhs]) }
+}
+
 impl ops::Neg for Color {
     type Output = Color;
     fn neg(self) -> Color { Color([1.0 - self.0[0], 1.0 - self.0[1], 1.0 - self.0[2]]) }
+}
+
+impl iter::Sum for Color {
+    fn sum<I>(iter: I) -> Color
+        where I: Iterator<Item=Color>
+    {
+        iter.fold(Color::gray(0.0), |a, x| a + x)
+    }
 }
 
 impl fmt::Display for Color {
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@ extern crate blake2;
 extern crate rand;
 extern crate png;
 extern crate nalgebra as na;
+extern crate data_encoding;
 
 use std::fs::File;
 use std::path::Path;
 use std::io::BufWriter;
+use std::mem;
 use clap::{Arg, App};
 use blake2::{Blake2b, Digest};
 use rand::{Rng, SeedableRng, XorShiftRng, Closed01};
-use rand::distributions::{Range, Gamma, IndependentSample};
+use rand::distributions::{Weighted, WeightedChoice, Range, Gamma, IndependentSample};
 use png::HasParameters;
+use data_encoding::HEXUPPER;
 
 mod expr;
 
@@ -25,6 +28,13 @@ fn main() {
     fn check_float(x: String) -> Result<(), String> {
         x.parse().map(|_: f32| ()).map_err(|e| e.to_string())
     }
+    fn check_hex(x: String) -> Result<(), String> {
+        if HEXUPPER.decode_len(x.as_bytes().len()).map_err(|e| e.to_string())? != 16 {
+            return Err("incorrect length; should be 32 hex digits".into());
+        }
+        let mut tmp = [0; 16];
+        HEXUPPER.decode_mut(x.as_bytes(), &mut tmp).map(|_| ()).map_err(|e| e.error.to_string())
+    }
 
     let args = App::new("rustart")
         .version("0.1")
@@ -33,8 +43,17 @@ fn main() {
         .arg(Arg::with_name("FILE")
              .help("File to write PNG output to.")
              .required(true))
-        .arg(Arg::with_name("SEED")
-             .help("String to seed the RNG with. If unsupplied, a random seed is used."))
+        .arg(Arg::with_name("seed")
+             .short("s")
+             .long("seed")
+             .help("String to seed the pRNG with. If unsupplied, a random seed is used."))
+        .arg(Arg::with_name("raw seed")
+             .short("r")
+             .long("raw-seed")
+             .validator(check_hex)
+             .help("Raw 128-bit pRNG seed")
+             .takes_value(true)
+             .conflicts_with("seed"))
         .arg(Arg::with_name("width")
              .short("w")
              .validator(check_nat)
@@ -64,19 +83,27 @@ fn main() {
     encoder.set(png::ColorType::RGB).set(png::BitDepth::Eight);
     let mut encoder = encoder.write_header().expect("couldn't write png header");
 
-    let seed = if let Some(text) = args.value_of("SEED") {
+    let seed = if let Some(text) = args.value_of("raw seed") {
+        let mut seed: [u8; 16] = unsafe { mem::uninitialized() };
+        HEXUPPER.decode_mut(text.as_bytes(), &mut seed).unwrap();
+        unsafe { mem::transmute::<[u8; 16], [u32; 4]>(seed) }
+    } else if let Some(text) = args.value_of("seed") {
         let mut hasher = Blake2b::default();
         hasher.input(text.as_bytes());
         let x = hasher.result();
+        println!("raw seed: {}", HEXUPPER.encode(&x));
         unsafe { *(x.as_ptr() as *const [u32; 4]) }
     } else {
-        rand::random()
+        let seed: [u8; 16] = rand::random();
+        println!("raw seed: {}", HEXUPPER.encode(&seed));
+        unsafe { mem::transmute::<[u8; 16], [u32; 4]>(seed) }
     };
 
     let mut rng = XorShiftRng::from_seed(seed);
     let mut data = vec![Color([0.0; 3]); (width*height) as usize];
-    let expr = generate(&mut rng, depth);
-    println!("generating {}", expr);
+    let expr = generate(&mut rng, depth).simplify();
+    println!("program: {}", expr);
+    println!("generating");
     render(expr, &mut data, width as usize, height as usize, zoom);
 
     println!("encoding");
@@ -91,7 +118,7 @@ fn flatten(xs: &[[u8; 3]]) -> &[u8] {
 }
 
 fn convert(x: f32) -> u8 {
-    let x = if x <= 0.0031308 { 12.92 * x } else { 1.055 * x.powf(1.0 / 2.4) - 0.055 };
+    let x = if x <= 0.0031308 { 12.92 * x } else { 1.055 * x.powf(1.0 / 2.4) - 0.055 }; // linear to gamma
     let x = if x <= 0.0 { 0.0 } else if x >= 1.0 { 1.0 } else { x };
     (x * 255.0) as u8
 }
@@ -108,15 +135,36 @@ fn render(e: Expr, out: &mut [Color], width: usize, height: usize, zoom: f32) {
 }
 
 fn generate<R: Rng>(rng: &mut R, depth: u32) -> Expr {
+    let mut leaf_weights = [
+        Weighted { weight: 3, item: 0 },
+        Weighted { weight: 1, item: 1 },
+        Weighted { weight: 1, item: 2 },
+        Weighted { weight: 1, item: 3 },
+    ];
+    let leaves = WeightedChoice::new(&mut leaf_weights);
+    let mut inner_weights = [
+        Weighted { weight: 1, item: 0 },
+        Weighted { weight: 4, item: 1 },
+        Weighted { weight: 1, item: 2 },
+        Weighted { weight: 1, item: 3 },
+        Weighted { weight: 2, item: 4 },
+    ];
+    let inner = WeightedChoice::new(&mut inner_weights);
+    generate_(rng, depth, inner, leaves)
+}
+
+fn generate_<R: Rng, I: IndependentSample<u32>, L: IndependentSample<u32>>(rng: &mut R, depth: u32, inner: I, leaves: L) -> Expr {
     use Expr::*;
     if depth == 0 {
-        match Range::new(0, 2).ind_sample(rng) {
+        match leaves.ind_sample(rng) {
             0 => Constant(rng.gen()),
             1 => Radial,
+            2 => Square,
+            3 => Cos,
             _ => unreachable!(),
         }
     } else {
-        match Range::new(0, 4).ind_sample(rng) {
+        match inner.ind_sample(rng) {
             0 => {
                 let Closed01(r): Closed01<f32> = rng.gen();
                 let rot = na::Rotation2::new(r * 2.0 * ::std::f32::consts::PI);
@@ -133,6 +181,10 @@ fn generate<R: Rng>(rng: &mut R, depth: u32) -> Expr {
             1 => Multiply(Box::new(generate(rng, depth - 1)), Box::new(generate(rng, depth - 1))),
             2 => Invert(Box::new(generate(rng, depth - 1))),
             3 => Tile(Box::new(generate(rng, depth - 1))),
+            4 => {
+                let n = Range::new(2, 4).ind_sample(rng);
+                Average((0..n).map(|_| generate(rng, depth - 1)).collect())
+            }
             _ => unreachable!(),
         }
     }
